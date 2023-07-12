c2ad6742de48bb7cbfa17c4c9544d73bf508b0b1
Ethan Gill <1ethangill1@gmail.com>
2023-07-11 19:03:18 -0700

    updated inquirer prompts

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 inquirer
 .prompt([
     {
-        type: 'list',
+        name: 'text',
+        message: 'Enter up to three characters:',
+        validate: input => /^[a-zA-Z0-9]{0,3}$/.test(input),
+        filter: input => input.toUpperCase()
+      },
+      {
+        name: 'textColor',
+        message: 'Enter text color:',
+        validate: input => /^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/.test(input),
+        filter: input => input.replace('#', '')
+      },
+      {
         name: 'shape',
-        message: 'Choose your Shape!!',
-        choices: shapeChoices
-    }
-])
+        message: 'Choose a shape:',
+        type: 'list',
+        choices: ['circle', 'triangle', 'square']
+      },
+      {
+        name: 'shapeColor',
+        message: 'Enter shape color:',
+        validate: input => /^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/.test(input),
+        filter: input => input.replace('#', '')
+      }
+    ])
