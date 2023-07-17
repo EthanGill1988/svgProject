c027b7abc85300fc26ab11ada7ab69ed66964387
Ethan Gill <1ethangill1@gmail.com>
2023-07-16 17:19:29 -0700

    was able to generate...something

--- index.js
+++ index.js
@@ -28,3 +28,31 @@ inquirer
         filter: input => input.replace('#', '')
       }
     ])
+    .then(answers => {
+        // Handle user's answers
+    const svg = generateSVG(answers);
+
+    // Write SVG to file
+    const filename = 'logo.svg';
+    fs.writeFile(filename, svg, err => {
+      if (err) {
+        console.error('Error creating SVG file:', err);
+        return;
+      }
+      console.log(`Generated ${filename}`);
+    });
+  })
+  .catch(err => {
+    console.error('Error:', err);
+  });
+
+  // Helper function to generate SVG content
+function generateSVG(answers) {
+  const { text, textColor, shape, shapeColor } = answers;
+  const svgTemplate = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+    <rect width="100%" height="100%" fill="#${shapeColor}" />
+    <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" fill="#${textColor}" font-size="50">${text}</text>
+  </svg>`;
+
+  return svgTemplate;
+}
